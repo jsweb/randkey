11ec8d29c4dbf82a4d23064ed2cd6b6cf6046a95
Alex Bruno Cáceres <git.alexbr@outlook.com>
2020-09-14 14:59:31 -0300

    Dependencies update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @desc Simple JS module to generate random id/key/hash in various formats, including UUID v4
  * @author Alex Bruno Cáceres <git.alexbr@outlook.com>
  * @create date 2016-06-26 03:21:18
- * @modify date 2020-09-14 17:56:29
+ * @modify date 2020-09-14 17:59:35
  */
 const rfc = [8, 9, 'a', 'b'];
 const pick = (arr = []) => arr[Math.floor(Math.random() * arr.length)];
